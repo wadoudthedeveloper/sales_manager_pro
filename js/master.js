3163804e0b1944470fc5b90e17d7ece7967e0428
wadoudthedeveloper <wadoudthedeveloper@gmail.com>
2023-01-02 09:49:18 +0100

    upload signup page

--- js/master.js
+++
@@ -1,9 +0,0 @@
-$(() => {
-  $(".shower_hider").on("mouseover", () => {
-    $(".hidden-menu").css("display", "block");
-  });
-
-  $(".hidden-menu").on("mouseout", () => {
-    $(".hidden-menu").css("display", "none");
-  });
-});
